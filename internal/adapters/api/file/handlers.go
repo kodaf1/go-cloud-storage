1b8a9e762c9de761a66f6339ccaf4b4107f715e5
Kodaf <contact@kodaf.me>
2022-05-31 17:26:05 +0300

    handler example

--- internal/adapters/api/file/handlers.go
+++ internal/adapters/api/file/handlers.go
@@ -1,8 +1,14 @@
 package user
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/kodaf1/go-cloud-storage/internal/adapters/api"
 	"github.com/kodaf1/go-cloud-storage/internal/domain/file"
+	"github.com/kodaf1/go-cloud-storage/pkg/logging"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -16,13 +22,27 @@ func NewHandler(service file.Service) api.Handler {
 }
 
 func (h *handler) Register(router *httprouter.Router) {
-	panic("implement me")
+	router.GET("/file/:uuid", h.GetFile)
 }
 
-func (h *handler) UploadFile(router *httprouter.Router) {
-	panic("implement me")
+func (h *handler) GetFile(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	file, err := h.fileService.GetFile(context.Background(), params.ByName("uuid"))
+	if err != nil {
+		logging.GetLogger().Debug("file not found")
+		http.Error(w, "Not Found", 404)
+	}
+
+	jsonFile, err := json.Marshal(file)
+	if err != nil {
+		logging.GetLogger().Error("can't marshal file")
+		http.Error(w, "Server Internal Error", 500)
+	}
+
+	w.WriteHeader(200)
+	fmt.Fprint(w, jsonFile)
+	logging.GetLogger().Debug("file returned")
 }
 
-func (h *handler) GetFile(router *httprouter.Router) {
+func (h *handler) UploadFile(w http.ResponseWriter, r *http.Request) {
 	panic("implement me")
 }
