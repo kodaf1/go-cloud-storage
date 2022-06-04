87e05750c29697f52ba4462e98cdb5f97af53f77
mikhail 'kodaf' moskalyov <contact@kodaf.me>
2022-06-04 06:02:21 +0300

    add url to file model

--- internal/domain/file/model.go
+++ internal/domain/file/model.go
@@ -3,5 +3,6 @@ package file
 type File struct {
 	UUID     string `json:"uuid" bson:"_id,omitempty"`
 	FileName string `json:"filename" bson:"file_name"`
+	URL      string `json:"url,omitempty"`
 	Size     int64  `json:"size" bson:"size"`
 }
--- internal/domain/file/service.go
+++ internal/domain/file/service.go
@@ -2,16 +2,18 @@ package file
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 )
 
 type service struct {
-	storage Storage
-	s3      S3
+	storage   Storage
+	s3        S3
+	URLPrefix string
 }
 
-func NewService(storage Storage, s3 S3) Service {
-	return &service{storage, s3}
+func NewService(storage Storage, s3 S3, urlPrefix string) Service {
+	return &service{storage, s3, urlPrefix}
 }
 
 func (s *service) UploadFile(ctx context.Context, dto *UploadFileDTO) (*File, error) {
@@ -24,12 +26,19 @@ func (s *service) UploadFile(ctx context.Context, dto *UploadFileDTO) (*File, er
 
 	file := &File{
 		FileName: uid,
+		URL:      fmt.Sprintf("%s%s", s.URLPrefix, uid),
 		Size:     dto.File.Size,
 	}
 
 	return s.storage.Create(file)
 }
 
-func (s *service) GetFile(ctx context.Context, uuid string) (*File, error) {
-	return s.storage.GetOne(uuid)
+func (s *service) GetFile(ctx context.Context, uuid string) (file *File, err error) {
+	file, err = s.storage.GetOne(uuid)
+	if err != nil {
+		return file, err
+	}
+
+	file.URL = fmt.Sprintf("%s%s", s.URLPrefix, file.FileName)
+	return file, err
 }
